1729e2b5ab286fa84e9647182665c7622e9a0cff
Gianluca Fernandez <gianluca.fernandez@academytrainee.com>
2020-07-24 14:41:56 +0100

    Added Button tied to Types API and in process of implementing code

--- types.js
+++ types.js
@@ -1,3 +1,52 @@
 (() => {
+    let userUpperLimit;
+    let userLowerLimit;
+    let abilityBtn = document.querySelector("#TypeBtn");
+    
+    function sendValues() {
+      userLowerLimit = document.getElementById("userLowerLimit").value;
+      userUpperLimit = document.getElementById("userUpperLimit").value;
+      limit(userLowerLimit, userUpperLimit);
+      console.log("yo we getting intothis funtcion?");
+    }
+    
+    // Abilities
+    function limit(userLowerLimit, userUpperLimit) {
+      fetch(
+        "https://pokeapi.co/api/v2/type/?offset=" +
+          userLowerLimit +
+          "&limit=" +
+          (userUpperLimit)
+      ).then(function (response) {
+        if (response.status !== 200) {
+          console.log(
+            "Looks like there was a problem. Status Code: " + response.status
+          );
+          return;
+        }
+        response.json().then(function (data) {
+          console.log(data);
+          for (userLowerLimit < userUpperLimit;;) {
+            console.log(data.results[i].name);
+            console.log(data.results[i].url);
+    
+            let para = document.createElement("P"); // Create a <p> element
+            para;
+            para.className = "alert alert-danger col-md-8";
+            para.innerText = `The name is : ${data.results[i].name} \n
+            The url is : ${data.results[i].url}`; // Insert text
+    
+            let myDiv = document.getElementById("myDiv");
+    
+            myDiv.appendChild(para);
+          }
+        });
+      });
+    }
 
+    abilityBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        sendValues();
+    })
 })(window);
